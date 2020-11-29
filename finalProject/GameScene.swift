d9c656b96e1affaa7f35c7ba702f26ee5ed2bd5c
Owen Zhang <owenzhang76@gmail.com>
2020-11-29 17:26:16 -0600

    second commit

--- finalProject/GameScene.swift
+++ finalProject/GameScene.swift
@@ -11,11 +11,93 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    //Thanks to https://www.hackingwithswift.com/example-code/games/how-to-create-a-random-terrain-tile-map-using-sktilemapnode-and-gkperlinnoisesource  and https://www.raywenderlich.com/1079-what-s-new-in-spritekit-on-ios-10-a-look-at-tile-maps for inspiration.
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    // touch location
+    var targetLocation: CGPoint = .zero
+    
+    // Scene Nodes
+    var car:SKSpriteNode!
+    var nodePosition = CGPoint()
+    var startTouch = CGPoint()
+    
+    let map = SKNode()
+    
+    func makeNoiseMap(columns: Int, rows: Int) -> GKNoiseMap {
+        let source = GKPerlinNoiseSource()
+        source.persistence = 0.85
+
+        let noise = GKNoise(source)
+        let size = vector2(1.0, 1.0)
+        let origin = vector2(0.0, 0.0)
+        let sampleCount = vector2(Int32(columns), Int32(rows))
+
+        return GKNoiseMap(noise, size: size, origin: origin, sampleCount: sampleCount, seamless: true)
+    }
+    
+    func loadSceneNodes() {
+        
+        guard let car = childNode(withName: "car") as? SKSpriteNode else {
+            fatalError("Sprite Nodes not loaded")
+        }
+        self.car = car
+        let cameraNode = SKCameraNode()
+        cameraNode.position = CGPoint(x: 0, y: 0)
+        self.addChild(cameraNode)
+        self.camera = cameraNode
+    }
+    
     override func didMove(to view: SKView) {
         
+        loadSceneNodes()
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        addChild(map)
+        map.xScale = 0.2
+        map.yScale = 0.2
+        
+        let tileSet = SKTileSet(named: "Sample Grid Tile Set")!
+        let tileSize = CGSize(width: 128, height: 128)
+        let columns = 128
+        let rows = 128
+        
+        let waterTiles = tileSet.tileGroups.first { $0.name == "Water" }
+        let grassTiles = tileSet.tileGroups.first { $0.name == "Grass"}
+        let sandTiles = tileSet.tileGroups.first { $0.name == "Sand"}
+        
+        let bottomLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+        bottomLayer.fill(with: sandTiles)
+        map.addChild(bottomLayer)
+        
+        // create the noise map
+        let noiseMap = makeNoiseMap(columns: columns, rows: rows)
+
+        // create our grass/water layer
+        let topLayer = SKTileMapNode(tileSet: tileSet, columns: columns, rows: rows, tileSize: tileSize)
+
+        // make SpriteKit do the work of placing specific tiles
+        topLayer.enableAutomapping = true
+
+        // add the grass/water layer to our main map node
+        map.addChild(topLayer)
+        
+        for column in 0 ..< columns {
+            for row in 0 ..< rows {
+                let location = vector2(Int32(row), Int32(column))
+                let terrainHeight = noiseMap.value(at: location)
+
+                if terrainHeight < 0 { //Can be modified depending on how much water we want. Can even add conditions on height to create different tiles in splotchy pattern.
+                    topLayer.setTileGroup(waterTiles, forColumn: column, row: row)
+                } else {
+                    topLayer.setTileGroup(grassTiles, forColumn: column, row: row)
+                }
+            }
+        }
+        
+
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
@@ -54,24 +136,29 @@ class GameScene: SKScene {
         }
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            startTouch = location
+            nodePosition = car.position
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+            car.run(SKAction.move(to: CGPoint(x:  nodePosition.x + location.x - startTouch.x, y: nodePosition.y + location.y - startTouch.y), duration: 0.2)) //Duration changes character spped.
+            camera?.position.x = car.position.x
+            camera?.position.y = car.position.y
+        }
+    }
+    
     func touchUp(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.red
             self.addChild(n)
         }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -86,4 +173,6 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+
 }
