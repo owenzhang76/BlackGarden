1aad2de63853a8abe4efc86fceca428101ae2371
Marissa Kalkar <mkalkar@wustl.edu>
2020-12-06 17:32:56 -0600

    player class update

---
+++ finalProject/Player.swift
@@ -0,0 +1,46 @@
+//
+//  Player.swift
+//  finalProject
+//
+//  Created by Marissa Kalkar on 12/6/20.
+//  Copyright © 2020 Tory Farmer. All rights reserved.
+//
+
+import UIKit
+
+class Player:CharacterProtocol {
+    var health:Int
+    var location = vector2(Int32(), Int32())
+    var speed:Double
+    var damage:Int
+    var image = SKSpriteNode()
+    var array = [SKTexture]()
+    var position:CGPoint
+    var items = [Item]()
+    
+    init(fromHealth health:Int, fromLocation location:simd_int2, fromSpeed speed:Double, fromDamage damage:Int, fromPosition position:CGPoint, fromItems items: [Item]) {
+        self.health = health
+        self.location = location
+        self.speed = speed
+        self.damage = damage
+        self.position = position
+        self.items = items
+    }
+    
+    func appear() {
+        
+    }
+    
+    func wonder() {
+        
+    }
+    
+    func attack(another:SKNode) {
+        
+    }
+    
+    func die() {
+        
+    }
+}
+
---
+++ finalProject/PlayerClass.swift
@@ -0,0 +1,9 @@
+//
+//  PlayerClass.swift
+//  finalProject
+//
+//  Created by Marissa Kalkar on 12/6/20.
+//  Copyright © 2020 Tory Farmer. All rights reserved.
+//
+
+import Foundation
