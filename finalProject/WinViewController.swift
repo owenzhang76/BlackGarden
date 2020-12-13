f69b22ba20c8f8ed16b2e20559aa9e6f924a3860
Marissa Kalkar <mkalkar@wustl.edu>
2020-12-13 16:03:48 -0600

    storyboard and layout fixes

--- finalProject/WinViewController.swift
+++ finalProject/WinViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class WinViewController: UIViewController {
 
+    //help from https://stackoverflow.com/questions/27153181/how-do-you-make-a-background-image-scale-to-screen-size-in-swift  
     override func viewDidLoad() {
         print("In win view controller")
         super.viewDidLoad()
+        //self.view.backgroundColor = UIColor(patternImage: UIImage(named:"black_garden_launch_image.png")!)
 
+        self.view.addBackground()
         // Do any additional setup after loading the view.
     }
     
@@ -29,3 +32,19 @@ class WinViewController: UIViewController {
     */
 
 }
+
+extension UIView {
+func addBackground() {
+    // screen width and height:
+    let width = UIScreen.main.bounds.size.width
+    let height = UIScreen.main.bounds.size.height
+
+    let imageViewBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+    imageViewBackground.image = UIImage(named: "black_garden_launch_image.png")
+
+    // you can change the content mode:
+    imageViewBackground.contentMode = UIView.ContentMode.scaleAspectFill
+
+    self.addSubview(imageViewBackground)
+    self.sendSubviewToBack(imageViewBackground)
+}}
